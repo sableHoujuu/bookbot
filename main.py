524d9954b84d095ee96accf8a785857bb3676458
sableHoujuu <dktpoole3@gmail.com>
2025-12-13 14:38:09 -0500

    moved function from main into new stats.py module

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+from stats import get_word_count
+
+
 def get_book_text(
     book_path,
 ):  # Takes a filepath as input and returns contents as string
     with open(book_path) as book:
         book_contents = book.read()
         return book_contents
-
-
-def get_word_count(text):
-    words = text.split()
-    return len(words)
 
 
 def main():
